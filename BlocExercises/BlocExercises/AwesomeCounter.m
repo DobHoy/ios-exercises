2b51f2580fd1ef17e72f39aac5d6815e7bb08559
Sri Mohan <dob.hoy@gmail.com>
2015-03-26 14:52:33 +0000

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,28 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+  
+    NSMutableString *outputString = [@"" mutableCopy];
+
+    long smallNumber, largeNumber;
+    long diffNumbers = (long) number - (long) otherNumber;
+    if (diffNumbers > 0)
+        {
+            largeNumber = (long) number;
+            smallNumber = (long) otherNumber;
+        }
+    else{
+            largeNumber = (long) otherNumber;
+            smallNumber = (long) number;
+    }
+    
+    for (long i = (long) smallNumber; i <= largeNumber; i++) {
+        NSLog(@"the number is %ld", i);
+        [outputString appendString:[NSString stringWithFormat:@"%ld", i]];
+    }
+    
+    
+    return outputString;
 }
 
 @end
