7c5e3ca7a57c8ffa9e49b8b049cd8db92a8214f9
Sri Mohan <dob.hoy@gmail.com>
2015-03-27 12:51:36 +0000

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,21 +12,49 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+    NSArray *returnArray = [characterString componentsSeparatedByString:@";"];
+    
+    return returnArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSString *returnString = [characterArray componentsJoinedByString:@";"];
+    
+    return returnString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    //Create a mutable array
+    NSMutableArray *bufferArray = [characterArray mutableCopy];
+    
+    //sort it
+    [bufferArray sortUsingDescriptors:@[sortDescriptor]];
+    
+    //and return it as NSMutableArray is a subclass of NSArray anyway
+    return bufferArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+
+    for(NSString *testCase in characterArray)
+    {
+        NSLog(@"test case is %@", testCase);
+        //if test case contains Worf, bool is true
+        
+        NSRange worfRange = [testCase rangeOfString:@"worf" options:NSCaseInsensitiveSearch];
+        if (worfRange.location != NSNotFound) {
+            return YES;
+        }
+    }
+    
+    
     return NO;
 }
 
